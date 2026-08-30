b7db67cd652f16ddbd01df437752746331d0b6f5
allenabad <149460428+allenabad@users.noreply.github.com>
2026-08-30 14:06:00 +0800

    fix: scroll to top on route change to prevent browser scroll restoration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import { Layout } from "./components";
 import { Home, Experience } from "./pages";
 import { ProjectsPage } from "./pages/Projects";
 import "./App.css";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 function App() {
   return (
     <HashRouter>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
